3e9c9260a8a8ad0bbeab917b37e086bc3b1e5f7e
MLife20 <ouabi36@gmail.com>
2021-01-02 17:50:46 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -3,7 +3,7 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m MSamy03
 sudo adduser MLife20 sudo
-echo 'MSamy03:8426' | sudo chpasswd
+echo 'MSamy03:0305' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
